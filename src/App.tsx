5d0da7103a5359b70b95908f926d69dafbb0e4ca
anros-p <37807776+anros-p@users.noreply.github.com>
2024-12-19 15:14:52 +0100

    Succeed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useStatew(0);
+    const [count, setCount] = useState(0);
 
     return (
         <>
